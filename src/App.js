3f18a8eedd404b890280a849c92ef74b7a220944
yoontaejin <nicetaejin@roseystems.kr>
2020-02-11 15:30:19 +0900

    2020-02-11 react공부소스

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    num : 0
+  }
+  plus = () => {
+    this.setState(count => ({num : count.num + 1}));
+  };
+  minus = () => {
+    this.setState(count => ({num : count.num -1}));
+  };
+  remove = () => {
+    this.setState(count => ({num : 0}));
+  };
+  render(){
+    return (
+      <div>
+       <h1>The Number = {this.state.num}</h1>
+       <button onClick={this.plus}>plus</button><br/>
+       <button onClick={this.minus}>minus</button><br/>
+       <button onClick={this.remove}>remove</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
